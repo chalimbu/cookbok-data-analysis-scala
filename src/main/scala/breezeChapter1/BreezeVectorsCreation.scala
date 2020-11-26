15d78fbf0366bd648bb8a2d06f0794265f28135f
sebastianzapata <sebastianzapata@seven4n.com>
2020-11-25 19:48:27 -0500

    se termina el ejercisio 1

--- src/main/scala/breezeChapter1/BreezeVectorsCreation.scala
+++ src/main/scala/breezeChapter1/BreezeVectorsCreation.scala
@@ -8,21 +8,25 @@ object BreezeVectorsCreation extends App{
   //crear una tupla
   // con el primer elemento un Vector de breeze con los valores 0,0,0,2,4
   //segundo elemento un vector con los valores 1,2,3,4
+  //solucion
 
  }
 
  def eje2()={
   //crear sparseVector con 5 false (NO EXPLICITAMENTE)
+  //solucion
 
  }
 
  def eje3()={
-  //crear un DenseVector con 10 registros cada uno indicando el index(NO EXPLICITAMENTE)
+  //crear un DenseVector con 10 registros Double cada uno indicando el index(NO EXPLICITAMENTE)
+  //solucion
 
  }
 
  def eje4()={
   //crear un dense vector apartir de la secuencia de scala
-
+  val secuencia=Seq(1,2,3,4,5,6)
+  //solucion
  }
 }
--- src/test/scala/breezeChapter/BreezeVectorCreationTest.scala
+++ src/test/scala/breezeChapter/BreezeVectorCreationTest.scala
@@ -6,11 +6,11 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class BreezeVectorCreationTest extends AnyFunSuite{
   test("retornos de funciones para el ejercisio 1"){
-    val vector=DenseVector(1,2,3,4,5)
+    val vector=DenseVector(1,2,3,4)
     val sparse= SparseVector(0,0,0,2,4)
     assert(BreezeVectorsCreation.eje1()==(sparse,vector))
-    assert(BreezeVectorsCreation.eje2()==SparseVector.zeros[Boolean](4))
-    assert(BreezeVectorsCreation.eje3()==DenseVector.tabulate[Double](5)(index=>index))
+    assert(BreezeVectorsCreation.eje2()==SparseVector.zeros[Boolean](5))
+    assert(BreezeVectorsCreation.eje3()==DenseVector.tabulate[Double](10)(index=>index))
     assert(BreezeVectorsCreation.eje4()==DenseVector(Seq(1,2,3,4,5,6)))
   }
 }
