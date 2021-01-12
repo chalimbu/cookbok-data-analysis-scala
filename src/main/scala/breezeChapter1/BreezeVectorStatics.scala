5f6fb9c1847c4d7dbbc8d492f353f48ffd0da134
sebastianzapata <sebastianzapata@seven4n.com>
2021-01-11 19:46:47 -0500

    se elimina la respuesta de los ejercisio

--- src/main/scala/breezeChapter1/BreezeVectorStatics.scala
+++ src/main/scala/breezeChapter1/BreezeVectorStatics.scala
@@ -8,20 +8,16 @@ object BreezeVectorStatics extends App{
   def eje1()={
     val evenNotsTill20Double: DenseVector[Double] =DenseVector[Double](0,20,2)
     //devolver una tupla de la media y la varianza del vector evenNotsTill20Double
-    val mv=meanAndVariance(evenNotsTill20Double)
-    (mv.mean,mv.variance)
   }
 
   def eje2()= {
     val evenNotsTill20Double: DenseVector[Double] = DenseVector[Double](0, 20, 2)
     //devolver el valor maximo en el vector evenNotsTill20Double
-    max(evenNotsTill20Double)
   }
 
   def eje3()={
     val evenNotsTill20Double: DenseVector[Double] = DenseVector[Double](0, 20, 2)
     //devolver una tupla aplicando a el vector raiz y logaritmo 10
-    (sqrt(evenNotsTill20Double),log(evenNotsTill20Double))
   }
 
 }
